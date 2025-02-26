a28e4514466d8fe53eb6cdc636582fa72abd9b70
matej-jurisic <mjurisic812@gmail.com>
2025-02-27 00:14:46 +0100

    feat: implement service response pattern

---
+++ backend/src/Operum.Model/Common/ServiceResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace Operum.Model.Common
+{
+    public class ServiceResponse
+    {
+        public string? Message { get; set; }
+        public bool? Success { get; set; }
+    }
+
+    public class ServiceResponse<T>
+    {
+        public bool? Success { get; set; }
+        public string? Message { get; set; }
+        public T Data { get; set; } = default!;
+
+        public static implicit operator ServiceResponse<T>(ServiceResponse response)
+        {
+            return new ServiceResponse<T> { Success = response.Success, Message = response.Message, Data = default! };
+        }
+    }
+}
