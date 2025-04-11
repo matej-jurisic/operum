5fd7796c8deb157d3c314a33b833977719076505
matej-jurisic <mjurisic812@gmail.com>
2025-04-11 13:52:01 +0200

    feat: migrate to dotnet 9.0

---
+++ backend/src/Operum.Model/Migrations/20250411111452_AddSomeMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Operum.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSomeMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "character varying(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
